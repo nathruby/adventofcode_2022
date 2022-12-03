d296af82b13202657751aad793ada5e82400ef92
Natalie Hruby <nataliemhruby@gmail.com>
2022-12-02 23:49:44 -0700

    abstracted priority score conversion

--- day_3/main.py
+++ day_3/main.py
@@ -11,42 +11,52 @@ def get_input():
     return file_input
 
 def part_1(rucksacks):
-    priority_score = 0
+    total_priority_score = 0
 
     for mixed_rucksack in rucksacks:
-        rucksack_1 = mixed_rucksack[0:int(len(mixed_rucksack)/2)]
+        rucksack_1 = mixed_rucksack[:int(len(mixed_rucksack)/2)]
         rucksack_2 = mixed_rucksack[int(len(mixed_rucksack)/2):]
 
         common_items = list(set(rucksack_1) & set(rucksack_2))
+        total_priority_score+=convert_to_priority_score(common_items[0])
 
-        if common_items[0].isupper():
-            priority_score+=ord(common_items[0])-38
-        else:
-            priority_score+=ord(common_items[0])-96
-
-    return priority_score
+    return total_priority_score
 
 def part_2(rucksacks):
-    priority_score = 0
+    total_priority_score = 0
 
     for i in range(0, len(rucksacks), 3):
         rucksack_1 = rucksacks[i]
         rucksack_2 = rucksacks[i+1]
         rucksack_3 = rucksacks[i+2]
-        
+
         common_items = list(set(rucksack_1) & set(rucksack_2) & set(rucksack_3))
+        total_priority_score+=convert_to_priority_score(common_items[0])
 
-        if common_items[0].isupper():
-            priority_score+=ord(common_items[0])-38
-        else:
-            priority_score+=ord(common_items[0])-96
+    return total_priority_score
 
-    return priority_score
+def convert_to_priority_score(item: str):
+    '''
+    Lowercase has a priority of at least 1 with a.
+    Uppercase has a priority of at least 27 with A.
+    '''
+
+    if item.isupper():
+        return ord(item)-38
+
+    return ord(item)-96
 
 if __name__ == '__main__':
     file_input = get_input()
     part_1_priority_score = part_1(file_input)
     part_2_priority_score = part_2(file_input)
-    
+
     print(part_1_priority_score)
     print(part_2_priority_score)
+
+#     statement= '''
+# file_input = get_input()
+# part_1_priority_score = part_1(file_input)
+# part_2_priority_score = part_2(file_input)
+# '''
+#     print(timeit.timeit(stmt=statement, globals=globals(), number=1000))
