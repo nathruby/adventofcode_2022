959c9dffe459d0a34bd2f90bf041adc687ec12c5
Natalie Hruby <nataliemhruby@gmail.com>
2022-12-04 01:30:07 -0700

    optimized removal of intersections for logical operations

--- day_4/main.py
+++ day_4/main.py
@@ -1,5 +1,6 @@
 '''Coding Challenge from https://adventofcode.com/2022/day/4'''
 from pathlib import Path
+import timeit
 
 filepath = Path(__file__).with_name('input.txt')
 
@@ -11,37 +12,35 @@ def get_input():
     return file_input
 
 def part_1(assignment_pairs):
-    
-    total_overlaps = 0
+
+    total_subsets = 0
 
     for assignment_pair in assignment_pairs:
         assignment_pair = assignment_pair.split(',')
-        group_1_range = assignment_pair[0].split('-')
-        group_2_range = assignment_pair[1].split('-')
+        group_1_range = [ int(value) for value in assignment_pair[0].split('-')]
+        group_2_range = [ int(value) for value in assignment_pair[1].split('-')]
 
-        group_1 = { i for i in range(int(group_1_range[0]), int(group_1_range[1])+1)}
-        group_2 = { i for i in range(int(group_2_range[0]), int(group_2_range[1])+1)}
+        if (group_2_range[0] <= group_1_range[0] and group_1_range[1] <= group_2_range[1])\
+            or (group_1_range[0] <= group_2_range[0] and group_2_range[1] <= group_1_range[1]):
+            total_subsets+=1
 
-        if group_1.issubset(group_2) or group_2.issubset(group_1):
-            total_overlaps+=1
-
-    return total_overlaps
+    return total_subsets
 
 def part_2(assignment_pairs):
-    total_overlaps = 0
+    total_intersects = 0
 
     for assignment_pair in assignment_pairs:
         assignment_pair = assignment_pair.split(',')
-        group_1_range = assignment_pair[0].split('-')
-        group_2_range = assignment_pair[1].split('-')
+        group_1_range = [ int(value) for value in assignment_pair[0].split('-')]
+        group_2_range = [ int(value) for value in assignment_pair[1].split('-')]
 
-        group_1 = set( i for i in range(int(group_1_range[0]), int(group_1_range[1])+1))
-        group_2 = set( i for i in range(int(group_2_range[0]), int(group_2_range[1])+1))
+        if (group_2_range[0] <= group_1_range[0] and group_1_range[0] <= group_2_range[1])\
+            or (group_2_range[0] <= group_1_range[1] and group_1_range[1] <= group_2_range[1])\
+            or (group_1_range[0] <= group_2_range[0] and group_2_range[0] <= group_1_range[1])\
+            or (group_1_range[0] <= group_2_range[1] and group_2_range[1] <= group_1_range[1]):
+            total_intersects+=1
 
-        if group_1 & group_2:
-            total_overlaps+=1
-
-    return total_overlaps
+    return total_intersects
 
 if __name__ == '__main__':
     file_input = get_input()
@@ -52,7 +51,7 @@ if __name__ == '__main__':
     print(part_2_results)
 #     statement= '''
 # file_input = get_input()
-# part_1_priority_score = part_1(file_input)
-# part_2_priority_score = part_2(file_input)
+# part_1_results = part_1(file_input)
+# part_2_results = part_2(file_input)
 # '''
 #     print(timeit.timeit(stmt=statement, globals=globals(), number=1000))
