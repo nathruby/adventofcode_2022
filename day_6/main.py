ac338238cc367c733c08f0577888471389735f48
Natalie Hruby <nataliemhruby@gmail.com>
2022-12-05 23:09:58 -0700

    Combind the two functions to one

--- day_6/main.py
+++ day_6/main.py
@@ -11,40 +11,29 @@ def get_input():
 
     return file_input[0]
 
-def part_1(input_stream):
-    start_of_packet_marker = 3 #best cast scenario, the first 4 characters are unique
+def find_starting_marker(input_stream, distinct_values):
 
-    for beginning_index in range(len(input_stream)+1):
-        start_of_packet_marker+=1
+    starting_marker: int
 
-        if len(set(input_stream[beginning_index:beginning_index+4]))==4:
-            return start_of_packet_marker
+    for ending_index in range(distinct_values, len(input_stream)+1):
+        starting_marker = ending_index
 
-    return start_of_packet_marker
+        if len(set(input_stream[ending_index-distinct_values:ending_index]))==distinct_values:
+            break
 
-def part_2(input_stream):
-
-    start_of_message_marker = 13 #best cast scenario, the first 13 characters are unique
-
-    for beginning_index in range(len(input_stream)+1):
-        start_of_message_marker+=1
-        
-        if len(set(input_stream[beginning_index:beginning_index+13]))==13:
-            return start_of_message_marker
-
-    return start_of_message_marker
+    return starting_marker
 
 if __name__ == '__main__':
     file_input = get_input()
-    part_1_results = part_1(file_input)
-    part_2_results = part_2(file_input)
+    part_1_results = find_starting_marker(file_input, 4)
+    part_2_results = find_starting_marker(file_input, 14)
 
     print(part_1_results)
     print(part_2_results)
 
 #     statement= '''
 # file_input = get_input()
-# part_1_results = part_1(file_input)
-# part_2_results = part_2(file_input)
+# part_1_results = find_starting_marker(file_input, 4)
+# part_2_results = find_starting_marker(file_input, 14)
 # '''
 #     print(timeit.timeit(stmt=statement, globals=globals(), number=1000))
