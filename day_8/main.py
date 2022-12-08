6864983bd16268bf9d83fc9e1c81828bb0131df3
Natalie Hruby <nataliemhruby@gmail.com>
2022-12-08 10:44:00 -0700

    Completed Day 8

--- day_8/main.py
+++ day_8/main.py
@@ -1,6 +1,7 @@
 '''Coding Challenge from https://adventofcode.com/2022/day/8'''
 from pathlib import Path
 import timeit
+from itertools import takewhile
 
 filepath = Path(__file__).with_name('input.txt')
 
@@ -9,18 +10,82 @@ def get_input():
     with filepath.open('r', encoding='utf-8') as file:
         file_input =  [line.rstrip('\n') for line in file]
 
-    return file_input
+    return [[int(tree) for tree in list(tree_row)] for tree_row in file_input]
 
-def part_1(assignment_pairs):
+def part_1(tree_layout):
 
-    total_subsets = 0
+    visible_trees = 0
+    transposed_tree_layout = [ list(column) for column in zip(*tree_layout)]
 
-    return total_subsets
+    for row_index,tree_row in enumerate(tree_layout):
+        for column_index,tree in enumerate(tree_row):
+            tree_height = tree
 
-def part_2(assignment_pairs):
-    total_intersects = 0
-    
-    return total_intersects
+            #4 directions to check height
+            #North
+            if tree_height > max(transposed_tree_layout[column_index][:row_index],default=-1):
+                visible_trees+=1
+                continue
+
+            #South
+            if tree_height > max(transposed_tree_layout[column_index][row_index+1:],default=-1):
+                visible_trees+=1
+                continue
+
+            #East
+            if tree_height > max(tree_row[column_index+1:],default=-1):
+                visible_trees+=1
+                continue
+
+            #West
+            if tree_height > max(tree_row[:column_index],default=-1):
+                visible_trees+=1
+                continue
+
+    return visible_trees
+
+def part_2(tree_layout):
+
+    scenic_scores = []
+    transposed_tree_layout = [ list(column) for column in zip(*tree_layout)]
+
+    for row_index,tree_row in enumerate(tree_layout):
+        for column_index,tree in enumerate(tree_row):
+            tree_height = tree
+            north = 0
+            south = 0
+            east = 0
+            west = 0
+
+            #4 directions to check height
+            #North
+            #if tree_height > max(transposed_tree_layout[column_index][:row_index],default=-1):
+            north = len(take_until(tree_height, reversed(transposed_tree_layout[column_index][:row_index])))
+                
+            #South
+            #if tree_height > max(transposed_tree_layout[column_index][row_index+1:],default=-1):
+            south = len(take_until(tree_height, transposed_tree_layout[column_index][row_index+1:]))
+                
+            #East
+            #if tree_height > max(tree_row[column_index+1:],default=-1):
+            east = len(take_until(tree_height, tree_row[column_index+1:]))
+                
+            #West
+            #if tree_height > max(tree_row[:column_index],default=-1):
+            west = len(take_until(tree_height, reversed(tree_row[:column_index])))
+
+            scenic_scores.append(north*south*east*west)
+
+    return max(scenic_scores, default=-1)
+
+def take_until(value, tree_list):
+    visible_trees = []
+    for tree in tree_list:
+        visible_trees.append(tree)
+        if tree >= value:
+            break
+
+    return visible_trees
 
 if __name__ == '__main__':
     file_input = get_input()
